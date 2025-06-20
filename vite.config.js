74e5b79e04231aaba4cf82226655e0af831bc608
Nitish <nitishmandal687@gmail.com>
2025-06-20 12:45:11 +0530

    add the port number

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000, // Dev server will run on localhost:3000
+    strictPort: true, // If port 3000 is busy, it will throw instead of using another
+  },
+  preview: {
+    port: 8080, // `vite preview` will serve on localhost:8080
+  },
   plugins: [react()],
-})
+});
