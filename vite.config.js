4a8e49efd763e552d2ac8d1a5b0d650d5907858e
Nitish <nitishmandal687@gmail.com>
2025-06-20 12:50:59 +0530

    adding the ip

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 3000, // Dev server will run on localhost:3000
     strictPort: true, // If port 3000 is busy, it will throw instead of using another
   },
